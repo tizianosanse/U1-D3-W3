55f71b95167974aaba68eb1c962fe29fa13526ff
Tiziano <tizianosanseverino0@gmail.com>
2024-03-13 18:38:46 +0100

    third commit aggiunto css a li

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const button = document.querySelector("button");
 const list = document.getElementById("ul");
 const input = document.getElementById("text");
 button.onclick = function () {
-  const item = document.createElement("li");
+  if (input.value === "") {
+    alert("you dont write nothing...");
+  } else {
+    const item = document.createElement("li");
 
-  console.log(input.value);
-  item.innerText = input.value;
-  const butt = document.createElement("button");
-  butt.innerText = "X";
-  butt.onclick = function () {
-    item.remove();
-  };
-  item.appendChild(butt);
-  list.appendChild(item);
+    console.log(input.value);
+    item.innerText = input.value;
+    item.className = "li";
+    const butt = document.createElement("button");
+    butt.innerText = "X";
+    butt.className = "butt";
+    butt.onclick = function () {
+      item.remove();
+    };
+    item.appendChild(butt);
+    list.appendChild(item);
+  }
 };
